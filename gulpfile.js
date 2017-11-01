788fce505da132c5389a17421c767831712d1065
tylermiller94 <tylermiller94@gmail.com>
2017-11-01 02:11:30 -0700

    add short test and comment out old code for later rework.

--- gulpfile.js
+++ gulpfile.js
@@ -4,61 +4,69 @@ var source = require('vinyl-source-stream');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var utilities = require('gulp-util');
+var buildProduction = utilities.env.production;
 var del = require('del');
 var jshint = require('gulp-jshint');
-var babelify = require('babelify');
+var babelify = require("babelify");
 var lib = require('bower-files')({
-  overrides: {
-    bootstrap: {
-      main: [
-        'less/bootstrap.less',
-        'dist/css/bootstrap.css',
-        'dist/js/bootstrap.js',
-      ],
-    },
-  },
+  "overrides":{
+    "bootstrap" : {
+      "main": [
+        "less/bootstrap.less",
+        "dist/css/bootstrap.css",
+        "dist/js/bootstrap.js"
+      ]
+    }
+  }
 });
 var browserSync = require('browser-sync').create();
 
-var buildProduction = utilities.env.production;
+function createErrorHandler(name) {
+  return function (err) {
+    console.error('Error from ' + name + ' in compress task', err.toString());
+  };
+}
+// to make readable error messages
+//https://github.com/terinjokes/gulp-uglify/tree/master/docs/why-use-pump
 
-gulp.task('concatInterface', function () {
+gulp.task('concatInterface', function() {
   return gulp.src(['./js/*-interface.js'])
     .pipe(concat('allConcat.js'))
     .pipe(gulp.dest('./tmp'));
 });
 
-gulp.task('jsBrowserify', ['concatInterface'], function () {
+
+gulp.task('jsBrowserify', ['concatInterface'], function() {
   return browserify({ entries: ['./tmp/allConcat.js'] })
     .transform(babelify.configure({
-      presets: ['es2015'],
-    }))
+        presets: ["es2015"]
+      }))
     .bundle()
     .pipe(source('app.js'))
     .pipe(gulp.dest('./build/js'));
 });
 
-gulp.task('minifyScripts', ['jsBrowserify'], function () {
-  return gulp.src('./build/js/app.js')
+gulp.task("minifyScripts", ["jsBrowserify"], function(){
+  return gulp.src("./build/js/app.js")
     .pipe(uglify())
-    .pipe(gulp.dest('./build/js'));
+    .pipe(gulp.dest("./build/js"))
+    // .on('error', createErrorHandler('gulp.dest'));
 });
 
-gulp.task('clean', function () {
+gulp.task("clean", function(){
   return del(['build', 'tmp']);
 });
 
-gulp.task('build', ['clean'], function () {
+gulp.task("build", ['clean'], function(){
   if (buildProduction) {
     gulp.start('minifyScripts');
   } else {
     gulp.start('jsBrowserify');
   }
-
   gulp.start('bower');
 });
 
-gulp.task('jshint', function () {
+gulp.task('jshint', function(){
   return gulp.src(['js/*.js'])
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
@@ -68,33 +76,42 @@ gulp.task('bowerJS', function () {
   return gulp.src(lib.ext('js').files)
     .pipe(concat('vendor.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('./build/js'));
+    .pipe(gulp.dest('./build/js'))
 });
 
+
 gulp.task('bowerCSS', function () {
-  return gulp.src(lib.ext('css').files)
+  return gulp.src([...lib.ext('css').files, 'css/*.css'])
     .pipe(concat('vendor.css'))
     .pipe(gulp.dest('./build/css'));
 });
 
 gulp.task('bower', ['bowerJS', 'bowerCSS']);
 
-gulp.task('serve', function () {
+gulp.task('serve', function() {
   browserSync.init({
     server: {
-      baseDir: './',
-      index: 'index.html',
-    },
+      baseDir: "./",
+      index: "index.html"
+    }
   });
+
   gulp.watch(['js/*.js'], ['jsBuild']);
   gulp.watch(['bower.json'], ['bowerBuild']);
-
+  gulp.watch(['css/*.css'], ['cssBuild']);
+  gulp.watch(['*.html'], function(){
+    browserSync.reload();
+  });
 });
 
-gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function () {
+gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function(){
   browserSync.reload();
 });
 
-gulp.task('bowerBuild', ['bower'], function () {
+gulp.task('bowerBuild', ['bower'], function(){
+  browserSync.reload();
+});
+
+gulp.task("cssBuild", ['bowerCSS'], function() {
   browserSync.reload();
 });
--- js/doctor-lookup-interface.js
+++ js/doctor-lookup-interface.js
@@ -1,32 +1,9 @@
 import { DoctorLookup } from './../js/doctor-lookup.js';
 
-function clear() {
-  $('#results').empty();
-}
+$(document).ready(function(){
+  const doctorsResult = new DoctorLookup();
 
-$(document).ready(function () {
-  $('.slider').slider();
-  let doctors = new DoctorLookup();
-
-  $(document).ajaxStart(function () {
-    $('#load').css('display', 'block');
-  });
-
-  $(document).ajaxComplete(function () {
-    $('#load').css('display', 'none');
-  });
-
-  $('#issue').submit(function (event) {
-    event.preventDefault();
-    clear();
-    let issue = $('#issue-input').val();
-    doctors.issue(issue);
-  });
-
-  $('#name').submit(function (event) {
-    event.preventDefault();
-    clear();
-    let doctorName = $('#name-input').val();
-    doctors.doctorName(doctorName);
-  });
+  $(".btn").click(function(){
+    doctorsResult.getDoctors();
+  })
 });
